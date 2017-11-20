f58bac74eac50bdef9c6a1b84ce268b8a970e2d3
OvcharovAlexandr <alexandr.ov4arov@gmail.com>
2017-11-20 20:22:10 +0300

    edited all errors

--- Test.js
+++ Test.js
@@ -1,16 +1,46 @@
 
 class DynamicTable {
-    
-    constructor(InColCount = 1, InLineCount = 1) {
-        this.ColCount   = InColCount;
-        this.LineCount  = InLineCount;
+       
+    constructor(index) {
+        
+        this.ColCount   = 4;
+        this.LineCount  = 4;
+        
+        this.outerDiv = document.querySelector(".DynTableInd_" + index);
+        this.CurTable = this.outerDiv.querySelector(".DynTable__Table");
+        
+        this.DelColButton = this.outerDiv.querySelector(".DynTable__DelColumn");
+        this.DellineButton = this.outerDiv.querySelector(".DynTable__DelLine");
+        
+        this.AddColumnButton = this.outerDiv.querySelector(".DynTable__AddColumn");
+        this.AddLineButton = this.outerDiv.querySelector(".DynTable__AddLine");
+        
+        //Add buttons   
+        this.AddColumnButton.addEventListener('click', () => this.addColumn());
+        this.AddLineButton.addEventListener('click', () => this.addLine());
+        
+        //Del buttons
+        this.DelColButton.addEventListener('click', ()=> {
+                                                        this.DelColButton.style.visibility = 'hidden';
+                                                        this.delColumn();
+                                                    });
+        
+        this.DellineButton.addEventListener('click', ()=> {
+                                                            this.DellineButton.style.visibility = 'hidden';
+                                                            this.delLine();
+                                                    });
+        
+        //outer Div
+        this.outerDiv.addEventListener('mouseover', (event)=> this.checkMouseOverEvent(event));
+        this.outerDiv.addEventListener('mouseout', (event)=> this.checkMouseOutEvent(event));
+        
     }
     
     addColumn() {
 
         this.ColCount ++;
         
-        let rows = document.getElementsByClassName("DynTable__tr");
+        let rows = this.CurTable.getElementsByClassName("DynTable__Row");
         
         for(let row of rows) {
             
@@ -26,8 +56,7 @@ class DynamicTable {
         
         this.LineCount ++; 
         
-        let CurTable = document.querySelector(".DynTable");
-        let CurTBody = CurTable.firstElementChild;
+        let CurTBody = this.CurTable.firstElementChild;
         
         let FirstElement    = CurTBody.firstElementChild;
         let NewElement      = FirstElement.cloneNode(true);
@@ -38,13 +67,15 @@ class DynamicTable {
     
     delColumn() {
        
+        this.DelColButton.style.visibility = 'hidden';
+        
         if (this.ColCount == 1) {
             return;
         }
         
         this.ColCount --;
         
-        let rows = document.getElementsByClassName("DynTable__tr");
+        let rows = this.CurTable.getElementsByClassName("DynTable__Row");
         
         for(let row of rows) {
         
@@ -62,112 +93,52 @@ class DynamicTable {
         
         this.LineCount --;
         
-        let CurTable = document.querySelector(".DynTable");
-        let CurTBody = CurTable.firstElementChild;
+        let CurTBody = this.CurTable.firstElementChild;
         
         let LastElement = CurTBody.lastElementChild;
         
         CurTBody.removeChild(LastElement);    
     }
+     
+    checkMouseOverEvent(event) {
         
-}
-
-let DynTable = new DynamicTable(4, 4);
-
-let outerDiv = document.querySelector(".Ramka");
-outerDiv.addEventListener('click', checkEvent);
-outerDiv.addEventListener('mouseover', checkEvent);
-outerDiv.addEventListener('mouseout', checkEvent);
-
-DynTable.addColumn();
-DynTable.addLine();
-
-function checkEvent(e) {
-
-    if (e.type === 'click') {
-        
-        if (e.target.classList.contains('js-AddColumn')) {
+        if (event.target.classList.contains('DynTable__Cell')) { 
             
-            DynTable.addColumn(); 
-            
-        }else if (e.target.classList.contains('js-AddLine')) {
-            
-            DynTable.addLine();
-            
-        }else if (e.target.classList.contains('js-DelColumn')) {
-            
-            let DelColButton = document.querySelector(".js-DelColumn");
-            DelColButton.style.visibility = 'hidden';       
-            
-            DynTable.delColumn();
-            
-        }else if (e.target.classList.contains('js-DelLine')) {
-            
-            let DellineButton = document.querySelector(".js-DelLine");
-            DellineButton.style.visibility = 'hidden';
-            
-            DynTable.delLine();
-            
-        }        
-        
-    }else if(e.type === 'mouseover'){
-       
-        if (e.target.classList.contains('DynTable__Cell')) {
-            
-            //Обработка события наведения на ячейки (спозиционируем кнопки удаления)
-            let DelColButton = document.querySelector(".js-DelColumn");
-            
-            if (DynTable.ColCount === 1){
-                
-                DelColButton.style.visibility = 'hidden';
+            if (this.ColCount === 1){
+                this.DelColButton.style.visibility = 'hidden';
             }else {
-                
-                DelColButton.style.visibility = 'visible';
-                
-                let CurLeft = e.target.offsetLeft + 1;
-                DelColButton.style.left = CurLeft + 'px';    
+                this.DelColButton.style.visibility = 'visible';
+                this.DelColButton.style.left = event.target.offsetLeft + 'px';    
             }
             
-            let DellineButton = document.querySelector(".js-DelLine");
-            
-            if (DynTable.LineCount === 1){
-                
-                DellineButton.style.visibility = 'hidden';
-            
-            }else {
-                
-                DellineButton.style.visibility = 'visible';
-                
-                let CurTop = e.target.offsetTop + 1;
-                DellineButton.style.top = CurTop + 'px';
+            if (this.LineCount === 1){
+                this.DellineButton.style.visibility = 'hidden';
+            }else {                
+                this.DellineButton.style.visibility = 'visible';
+                this.DellineButton.style.top = event.target.offsetTop + 'px';
             }
+        
+        }else if (event.target.classList.contains('DynTable__DelButton')) {
             
-            
-        }else if (e.target.classList.contains('DelButton')) {
-            
-            e.target.style.visibility = 'visible';
+            event.target.style.visibility = 'visible';
             
         }
+    }
+    
+    checkMouseOutEvent(event) {
         
-    }else if(e.type === 'mouseout'){
-       
-        if (e.target.classList.contains('DelButton')) {
+        if(event.target.classList.contains('DynTable__Cell')) {
             
-            e.target.style.visibility = 'hidden';
+            this.DellineButton.style.visibility = 'hidden';
+            this.DelColButton.style.visibility = 'hidden';       
             
-        }else if(e.target.classList.contains('DynTable__Cell')) {
+        }else if(event.target.classList.contains('DynTable__DelButton')) {
         
-            console.log(e.type);
-            
-            let DellineButton = document.querySelector(".js-DelLine");
-            DellineButton.style.visibility = 'hidden';
-            
-            let DelColButton = document.querySelector(".js-DelColumn");
-            DelColButton.style.visibility = 'hidden';       
+           event.target.style.visibility = 'hidden'; 
             
         }
-        
-
-    }    
+    }
+               
 }
 
+let DynTable = new DynamicTable(1);
